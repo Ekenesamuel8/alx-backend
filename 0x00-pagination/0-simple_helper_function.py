7b5843644365a61176a52507b0ca2bfd81d9f7b4
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-08-23 19:22:28 +0100

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,12 +4,13 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Calculate the start and end indices for the range of items on a specific page.
-    
+    Calculate the start and end indices for the range of
+      items on a specific page.
+
     Args:
     - page (int): The current page number (1-indexed).
     - page_size (int): The number of items per page.
-    
+
     Returns:
     - tuple: A tuple containing the start index and the end index.
     """
