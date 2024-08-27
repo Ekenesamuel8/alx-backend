66120a8e4872ca6f9abe464a6a4f3654e10d206d
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-08-28 00:15:02 +0100

    flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic flask setup"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """create a route for the app in the web browser"""
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    """run the app"""
+    app.run()
